2abe906732bbc7fbabd3c1c8b9ebf49e6109bb70
= <armaansecond9@gmail.com>
2026-09-04 10:59:31 +0530

    updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     applicationId = "com.aistudio.vaultpass.zxqwej"
     minSdk = 24
     targetSdk = 36
-    versionCode = 10
-    versionName = "2.6.2"
+    versionCode = 11
+    versionName = "2.6.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
